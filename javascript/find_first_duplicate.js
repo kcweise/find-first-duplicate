48a484eb9e140320356dcd6afded8ce8f1299cc5
Keenan Weise <kcweise@gmail.com>
2024-06-27 13:16:46 -0500

    Finished

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,19 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let seen = new Set();
+
+  if (arr.length < 2){
+    return -1;
+  }
+    for (let i = 0; i < arr.length; i +=1) {
+       if (seen.has(arr[i])){
+        return arr[i];
+      }
+      seen.add(arr[i]);
+    }
+  return -1;
 }
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,5 +28,10 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
+/* Please add your pseudocode to this file
+We have to iterate through array indexes trying to find a match that index number. Starting at index [0].
+We have to compare the index to the numbers in subsequent indexes. Nested loop.
+After iterating through each index the nested iteration can start at current index + 1.
+*/
+
 // And a written explanation of your solution
